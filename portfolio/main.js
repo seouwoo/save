788c8263529363f7e06689c9f4949635d534d553
seou <duswo822@gmail.com>
2024-06-19 16:41:13 +0900

    auto commit

--- portfolio/main.js
+++ portfolio/main.js
@@ -180,37 +180,6 @@ svgWraps.forEach((svgWrap) => {
   });
 });
 
-////////////변경
-let backColor=document.querySelectorAll("[data-bgcolor]")
-//console.log(backColor)
-// backColor.forEach(function(one,two,three){
-//     one  --> backColor배열안의 요소들이 차례로 들어옴
-//     two  --> one의 변수안에 할당된 아이템의 index번호
-//     three -->backColor 원배열 자체
-// })
-
-
-backColor.forEach(function(item,index){
-    let prevBg=index == 0 ?"":backColor[index - 1].dataset.bgcolor
-    ScrollTrigger.create({
-        trigger:".wave2",
-        start:"top 50%",
-        end:"center 5%",
-        duration:1,
-        markers: true,
-        onEnter:function(){
-            gsap.to("body",{
-                backgroundColor:item.dataset.bgcolor
-            })
-        },
-        onLeaveBack:function(){
-            gsap.to("body",{
-                backgroundColor:prevBg
-            })
-        }
-    })
-})
-
 
 
 //텍스트의 사라지는 방향 애니
