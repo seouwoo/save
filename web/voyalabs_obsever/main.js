bdc8968af69d0706ffd0c252d754e1687916192e
seou <duswo822@gmail.com>
2024-06-20 12:35:48 +0900

    auto commit

--- web/voyalabs_obsever/main.js
+++ web/voyalabs_obsever/main.js
@@ -1,3 +1,6 @@
-let observer=new IntersectionObserver(function(){
-    
-})
+// let observer=new IntersectionObserver(callback함수,options)
+let observer=new IntersectionObserver(function(entry){
+    if(entry.isIntersection){
+        console.log("타이틀이보인다")
+    }    
+},options)
