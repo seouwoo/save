c8a59cb66cbe7f08b935a38a535d38d1a5387f94
seou <duswo822@gmail.com>
2024-06-20 17:13:08 +0900

    auto commit

--- portfolio/main.js
+++ portfolio/main.js
@@ -185,7 +185,69 @@ svgWraps.forEach((svgWrap) => {
 });
 
 
+LottieScrollTrigger({
+  target: "#animationWindow",
+  path:"./Panasonic2.lottie.json" ,
+  speed: "medium",
+  scrub: 2,
+});
 
+LottieScrollTrigger({
+  target: "#animationWindow2",
+  path:"./Animation.json" ,
+  speed: "medium",
+  scrub: 2,
+});
+
+function LottieScrollTrigger(vars) {
+  let playhead = {
+          frame: 0
+      },
+      target = gsap.utils.toArray(vars.target)[0],
+      speeds = {
+          slow: "+=2000",
+          medium: "+=1000",
+          fast: "+=500"
+      },
+      st = {
+          trigger: target,
+          pin: true,
+          start: "top top",
+          end: speeds[vars.speed] || "+=1000",
+          scrub: 1,
+      },
+      ctx = gsap.context && gsap.context(),
+      animation = lottie.loadAnimation({
+          container: target,
+          renderer: vars.renderer || "svg",
+          loop: false,
+          autoplay: false,
+          path: vars.path,
+          rendererSettings: vars.rendererSettings || {
+              preserveAspectRatio: "xMidYMid slice",
+          },
+      });
+  for (let p in vars) {
+      // let users override the ScrollTrigger defaults
+      st[p] = vars[p];
+  }
+  animation.addEventListener("DOMLoaded", function () {
+      let createTween = function () {
+          animation.frameTween = gsap.to(playhead, {
+              frame: animation.totalFrames - 1,
+              ease: "none",
+              onUpdate: () => animation.goToAndStop(playhead.frame, true),
+              scrollTrigger: st,
+          });
+          return () => animation.destroy && animation.destroy();
+      };
+      ctx && ctx.add ? ctx.add(createTween) : createTween();
+      // in case there are any other ScrollTriggers on the page and the loading of this Lottie asset caused layout changes
+      ScrollTrigger.sort();
+      ScrollTrigger.refresh();
+  });
+  return animation;
+}
 //텍스트의 사라지는 방향 애니
 
 gsap.to("[data-direct]", {
