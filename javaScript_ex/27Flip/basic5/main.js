4537d352c4fcf5288517e2614b4bcc68c71a3f7c
seou <duswo822@gmail.com>
2024-06-21 11:16:16 +0900

    auto commit

--- javaScript_ex/27Flip/basic5/main.js
+++ javaScript_ex/27Flip/basic5/main.js
@@ -1,49 +1,62 @@
 gsap.registerPlugin(Flip);
 
-//const=상수(정해진값)
-//const activeThumb;//오류뜸 (의미가없어서) let *변수로 써야함
-//const activeThumb=의미부여;   //이렇게 해야 오류 안뜸 
+
 const thumbs=document.querySelectorAll(".item");
 const modal=document.querySelector('.modal');
 const toggle=document.querySelector('.page3 button')
 const modalImage=modal.querySelector('.modal-image');
 const wrapper=document.querySelector('.grid-wrapper');
-
 let activeThumb;
 
 thumbs.forEach((thumb)=>{
-    thumb.addEventListener("click",()=>{
-        activeThumb= thumb;
-        thumb.classList.add("active-thumb");
-        thumb.dataset.flipId="img";
-        gsap.set(thumb,{opacity:0})
+  thumb.addEventListener("click",()=>{
+    activeThumb=thumb;
+    thumb.classList.add("active-thumb");
+    thumb.dataset.flipId="img";//<div class="item" data-flip-id="img">...</div>
+    gsap.set(thumb,{opacity:0})
 
-        const state=Flip.getState([thumb,modalImage],{
-            props:"borderRadius,aspectRatio,BoxShadow"
-        })
-        modalImage.querySelector("img").setAttribute("src",thumb.dataset.url)
-        modal.classList.add("active")
-        modalImage.style.display="block";
-
-        Flip.from(state,{
-            duration:0.25,
-            ease:"sin.inOut"
-        })
+    const state=Flip.getState([thumb,modalImage],{
+        props:"borderRadius,aspectRatio,boxShadow"
     })
-    
+
+    modalImage.querySelector("img").setAttribute("src",thumb.dataset.url)
+    modal.classList.add('active')
+    modalImage.style.display="block";
+
+    Flip.from(state,{
+        duration:0.25,
+        ease:"sin.inOut"
+    })
+  })
 })
 
-modal.addEventListener("click",()=>{
+modal.addEventListener('click',()=>{
     gsap.set(activeThumb,{opacity:1})
-    
     const state=Flip.getState([activeThumb,modalImage],{
-        props:"borderRadius,aspectRatio,BoxShadow"
+        props:"borderRadius,aspectRatio,boxShadow"
     })
-    modal.classList.remove("active");
+    modal.classList.remove("active")
     Flip.from(state,{
-        duration:1,
+        duration:0.25,
         absolute:true,
         ease:"sine,inOut",
+       onComplete:()=>{
+        modalImage.querySelector("img").setAttribute("src","")
+        activeThumb.classList.remove("active-thumb");
+       }
     })
 })
 
+
+toggle.addEventListener("click",()=>{
+    toggle.classList.toggle('gird-view-on');
+    let state=Flip.getState(".grid-wrapper, .item-wrapper")//F
+    wrapper.classList.toggle("stack")//L
+
+    Flip.from(state,{
+        absolute:true,
+        duration:0.5,
+        ease:"sine",
+        stagger:0.05
+    })
+})
--- web/voyalabs/main.js
+++ web/voyalabs/main.js
@@ -1,9 +1,59 @@
 //cursor
-
 const coords={x:0,y:0};
-
-const circles= document.querySelectorAll(".circle")
-const svg= document.querySelector(".cursor img")
+const circles=document.querySelectorAll(".circle");
+const svg=document.querySelector(".cursor img")
 let timer;
 
-svg.style.visibility = "hidden";
+svg.style.visibility="hidden";
+
+circles.forEach((circle, index)=>{
+    //console.log(circle)
+    circle.x=0;
+    circle.y=0;
+})
+
+window.addEventListener("mousemove",function(e){
+    coords.x=e.clientX;
+    coords.y=e.clientY;
+
+   clearTimeout(timer)
+
+   svg.style.visibility="hidden";
+   circles.forEach((circle)=>{
+    circle.style.display='block';
+})
+
+    
+
+    // timer=setTimeout(function(){},시간)//시간만큼 지나면 할일이 한번 실행됨
+    timer=setTimeout(function(){
+        circles.forEach((circle)=>{
+            circle.style.display='none';
+            svg.style.visibility="visible";
+            svg.style.display="block"
+        })
+    },500)
+})
+
+
+function animateCircles(){
+    let x=coords.x;
+    let y=coords.y;
+
+    circles.forEach((circle,index)=>{
+        circle.style.left=x +"px";
+        circle.style.top=y +"px";
+
+        circle.x=x;
+        circle.y=y;
+
+        let nextCircle=circles[index + 1] || circles [0]
+        x += (nextCircle.x - x) *0.1;
+        y += (nextCircle.y - y) *0.1;
+        console.log(x)
+    })
+
+   
+    requestAnimationFrame(animateCircles)
+}
+animateCircles()
