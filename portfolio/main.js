1f5747c21c8fc48c75dc247a1d4b07269a5db83f
seou <duswo822@gmail.com>
2024-06-21 15:30:46 +0900

    auto commit

--- portfolio/main.js
+++ portfolio/main.js
@@ -217,6 +217,7 @@ backColor.forEach(function(item,index){
         trigger:item,
         start:"top 0%",
         end:"top 0%",
+        duration:-1,
         markers: true,
         onEnter:function(){
             gsap.to(".sec",{
