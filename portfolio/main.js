9dbc9a0686395b9727493470513ac42324953530
seou <duswo822@gmail.com>
2024-06-21 16:46:36 +0900

    auto commit

--- portfolio/main.js
+++ portfolio/main.js
@@ -53,29 +53,7 @@ function updateProgress() {
   progressTimer = requestAnimationFrame(updateProgress);
 }
 updateProgress();
-///////////////////////////////////////////
-let btn = document.querySelectorAll(".btn");
-      let animationInProgress = false;
-      let animationId;
-      btn.addEventListener("click", (e) => {
-        const clickEffect = document.querySelector(".click-effect");
 
-        if (animationInProgress) {
-          clearTimeout(animationId);
-          clickEffect.classList.remove("effect");
-          void clickEffect.offsetWidth;
-        }
-
-        clickEffect.style.top = e.clientY + window.scrollY + "px";
-        clickEffect.style.left = e.clientX + window.scrollX + "px";
-        clickEffect.classList.add("effect");
-        animationInProgress = true;
-
-        animationId = setTimeout(() => {
-          clickEffect.classList.remove("effect");
-          animationInProgress = false;
-        }, 750);
-      });
 ///////////////////////////////////
 //돌아가는거
 gsap.to(".badge", {
