fdadc3663adc8fc8838fc3451d25fcd226b94785
seou <duswo822@gmail.com>
2024-07-01 16:59:17 +0900

    auto commit

--- portfolio/main.js
+++ portfolio/main.js
@@ -278,6 +278,30 @@ svgWraps2.forEach((svgWrap) => {
   });
 });
 
+
+const svgText = document.querySelector("#textOnPath1");
+const svgText2 = document.querySelector("#textOnPath2");
+console.log(svgText);
+
+
+gsap.fromTo(
+    [svgText, svgText2], {//두개를 잡을땐 동시에잡기
+        attr: {
+            startOffset: "0%"
+        },
+    }, {
+        attr: {
+            startOffset: "100%"
+        },
+        scrollTrigger: {
+            trigger: ".wave1",
+            start: "top top",
+            scrub: 1,
+            end: "+=100%",
+            pin:true
+        },
+    }
+);
 //data-bgcolor
 
 let backColor = document.querySelectorAll("[data-bgcolor]")
